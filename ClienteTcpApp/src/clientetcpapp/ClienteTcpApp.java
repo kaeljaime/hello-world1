3577e2a45b36ce614a19ee31287f020266bf0a84
kaeljaime <37301329+kaeljaime@users.noreply.github.com>
2018-03-12 11:28:43 -0500

    Actualizacion de estilo
    Se cambia el estilo de input de busqueda

--- ClienteTcpApp/src/clientetcpapp/ClienteTcpApp.java
+++ ClienteTcpApp/src/clientetcpapp/ClienteTcpApp.java
@@ -16,6 +16,7 @@ public class ClienteTcpApp {
      */
     public static void main(String[] args) {
         System.out.println("Control Desarrollo");
+        System.out.println("Se realizo un cambio grande");
     }
     
 }
